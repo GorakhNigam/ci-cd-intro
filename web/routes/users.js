3444fd6465fd098f4737f0b527eb943a37861de0
Gorakhnath Nigam <jighrt16@gmail.com>
2026-05-02 12:41:39 -0400

    Change users route to root path

--- web/routes/users.js
+++ web/routes/users.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/users', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource')
 })
 
 module.exports = router
+
+
